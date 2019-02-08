0dc9a3e85d724c8279d2bf490025a117b92a361d
R3l3ntl3ss <dev.91096@gmail.com>
2019-02-08 05:45:30 +0530

    Added flask-cors

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 import os
 
 from TwitterClient import TwitterClient
 from SentimentAnalysis import get_sentiment
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/')
+@cross_origin
 def index():
     return 'Home'
 
 
 @app.route('/api/<query>')
+@cross_origin
 def api(query):
 
     query_limit = int(os.getenv('QUERY_LIMIT'))
